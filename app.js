d02783d3b3b108cd8c9fb61b34655b8d3a27a130
forch0 <forchh.dev@gmail.com>
2025-04-29 17:48:29 +0800

    basic setup 1

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express from 'express';
+import {PORT} from './config/env.js';
 
-    
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log(`Server running at http://localhost:3000/`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
 });
 
 export default app;
